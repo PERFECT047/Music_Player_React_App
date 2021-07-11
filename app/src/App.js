85f8a8692aabbe56272dc90687b34b614156f84e
Siddhartha Das <siddhartha.das.min19@itbhu.ac.in>
2021-07-11 06:32:24 -0700

    Skeleton

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+//Adding Component
+import Player from "./Components/Player";
+import Song from "./Components/Song";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Song />
+      <Player />
+    </div>
+  );
+}
+
+export default App;
